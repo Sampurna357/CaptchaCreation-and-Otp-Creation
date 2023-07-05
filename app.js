e1c9039f9fba0b3406127f96c8000747a8810a45
Sampurna jena <100677782+Sampurna357@users.noreply.github.com>
2023-07-05 23:07:57 +0530

    today 05-07-2023

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ var otpGenerator=()=>{
           var temp=parseInt(Math.random()*10);
           otp=otp+temp;
         }
-        document.querySelector("#otp").value=" ";
-        
-        document.querySelector(".res").innerHTML=otp;
+       
+        var divtag=document.createElement('div');
+        divtag.setAttribute("class","event");
+        divtag.innerHTML=otp;
 
+        document.querySelector(".res").append(divtag);
+
+}
+var validate=(event)=>{
+       
+      if(event.keyCode>=48 && event.keyCode<=57){
+        return true;
+      }
+      else{
+  
+        return false;
+      }
+
+        
 }
